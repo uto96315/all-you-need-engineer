5a6a5b775ec0fa58e56ce3d3bf789f021df87057
Yuto Mabe <yuto96315mabe@gmail.com>
2023-03-06 17:49:49 +0900

    reactQueryの設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
